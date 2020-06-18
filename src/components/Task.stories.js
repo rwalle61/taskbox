e853ef67eba279e8fc898c8492add743b043a5d5
Richard Waller <Richard.Waller@ibm.com>
2020-06-18 15:58:41 +0100

    feat: complete step 7 (Addons)

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
-  // Our exports that end in "Data" are not stories.
+  decorators: [withKnobs],
   excludeStories: /.*Data$/,
 };
 
@@ -15,6 +16,11 @@ export const taskData = {
   title: 'Test Task',
   state: 'TASK_INBOX',
   updatedAt: new Date(2018, 0, 1, 9, 0),
+};
+
+const longTitleTaskData = {
+  ...taskData,
+  title: `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`,
 };
 
 const pinnedTaskData = {
@@ -32,7 +38,9 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-export const Default = () => <Task task={taskData} {...actionsData} />;
+export const Default = () => <Task task={object('task', taskData)} {...actionsData} />;
+
+export const LongTitle = () => <Task task={longTitleTaskData} {...actionsData} />;
 
 export const Pinned = () => <Task task={pinnedTaskData} {...actionsData} />;
 
